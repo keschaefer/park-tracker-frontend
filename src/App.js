fe869716fb13ace034c33d4f51cca4a67d7a8a02
keschaefer <keschaefer@gmail.com>
2018-12-10 18:53:32 -0700

    added parallax library and put into app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './Components/Header.js'
+import Footer from './Components/Footer.js'
+import Main from './Components/Main.js'
+import Card_Container from './Components/Card_Container.js'
+import Card from './Components/Card.js'
+import { Parallax } from 'react-parallax';
 
 class App extends Component {
+  constructor() {
+    super() 
+      this.state = {
+        image1: "https://images.pexels.com/photos/418831/pexels-photo-418831.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    }
+  }
+
   render() {
     return (
       <div className="App">
-       
+        <Header />
+        <Parallax
+        bgImage= {this.state.image1} strength= {700}>
+            <div style={{height:500}}>
+            </div>
+        </Parallax>
+        <Main />
       </div>
     );
   }
---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Header = () => {
+   return (
+      <div className= "Header">
+        <nav class="navbar navbar-light bg-light">
+             {/* <a class="navbar-brand" href="#">
+            <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" alt=""></img>
+            </a> */}
+            <p>PARK TRACKER</p>
+         </nav>
+      </div>
+   )
+}
+
+export default Header
