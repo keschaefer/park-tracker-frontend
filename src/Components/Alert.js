fb2aa20d6dd57b4220b99651c49f08272c867ada
keschaefer <keschaefer@gmail.com>
2019-01-09 11:48:58 -0700

    refactored, cleaned up some links

---
+++ src/Components/Alert.js
@@ -0,0 +1,18 @@
+import React from "react"
+import SweetAlert from 'react-bootstrap-sweetalert';
+
+const Alert = (props) => {
+return (
+//    <div>
+//    <button onClick={() => this.setState({ show: true })}>Alert</button>
+//    <SweetAlert
+//      show={this.state.show}
+//      title="Demo"
+//      text="SweetAlert in React"
+//      onConfirm={() => this.setState({ show: false })}
+//    />
+//  </div>
+   )
+}
+
+export default Alert
